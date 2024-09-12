8cc7b8b8418c3bd163b862a0e01634b10ccc808f
Chamod Rashmika <89768971+chamodRash@users.noreply.github.com>
2024-09-12 08:50:12 +0530

    my profile part is done

--- app/(user)/profile/page.tsx
+++ app/(user)/profile/page.tsx
@@ -1,5 +1,22 @@
+import { MyOrders } from "@/components/my-orders";
+import { MyProfile } from "@/components/my-profile";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+
 const UserProfile = () => {
-  return <div>My Profile</div>;
+  return (
+    <Tabs defaultValue="profile" className="w-full rounded-xl">
+      <TabsList>
+        <TabsTrigger value="profile">My Profile</TabsTrigger>
+        <TabsTrigger value="orders">My Orders</TabsTrigger>
+      </TabsList>
+      <TabsContent value="profile">
+        <MyProfile />
+      </TabsContent>
+      <TabsContent value="orders">
+        <MyOrders />
+      </TabsContent>
+    </Tabs>
+  );
 };
 
 export default UserProfile;
