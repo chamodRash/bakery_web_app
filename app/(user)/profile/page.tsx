f2a7e5d7ade4c8c0bfcc3bc8169e76c554ca5c86
Chamod Rashmika <89768971+chamodRash@users.noreply.github.com>
2024-09-12 12:03:21 +0530

    Edited my profile page

--- app/(user)/profile/page.tsx
+++ app/(user)/profile/page.tsx
@@ -1,21 +1,10 @@
-import { MyOrders } from "@/components/my-orders";
 import { MyProfile } from "@/components/my-profile";
-import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 
 const UserProfile = () => {
   return (
-    <Tabs defaultValue="profile" className="w-full rounded-xl">
-      <TabsList>
-        <TabsTrigger value="profile">My Profile</TabsTrigger>
-        <TabsTrigger value="orders">My Orders</TabsTrigger>
-      </TabsList>
-      <TabsContent value="profile">
-        <MyProfile />
-      </TabsContent>
-      <TabsContent value="orders">
-        <MyOrders />
-      </TabsContent>
-    </Tabs>
+    <div className="w-full ">
+      <MyProfile />
+    </div>
   );
 };
 
