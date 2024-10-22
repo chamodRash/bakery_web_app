fb3a0f2f5b60242306d568b15d4db91c3d213500
sankavimohan <133623933+sankavimohan@users.noreply.github.com>
2024-10-23 02:01:08 +0530

    trying to fix build errors

--- app/admin/layout.tsx
+++ app/admin/layout.tsx
@@ -1,5 +1,5 @@
 import { Inter } from "next/font/google";
-import "./globals.css";
+import "@/app/globals.css";
 import AdminSidebar from "@/components/admin/sidebar";
 
 const inter = Inter({ subsets: ["latin"] });
