33103fa846f2e31a468c4ac58e699780835048b2
Chamod Rashmika <cnilwakka@gmail.com>
2024-12-15 13:35:47 +0530

    update stock page

--- app/admin/stock/page.tsx
+++ app/admin/stock/page.tsx
@@ -8,20 +8,18 @@ import { Button } from "@/components/ui/button";
 import { getStock } from "@/data/stock";
 import StockTable from "@/components/admin/stock-table";
 import AddStock from "@/components/admin/add-stock";
+import { Input } from "@/components/ui/input";
+import StockSearchBar from "@/components/admin/stock-searchbar";
 
 const StockPage = async () => {
   const stock = await getStock();
-
-  const deleteStock = (stockID: string) => {
-    // Delete categories
-    console.log("Delete categories");
-  };
 
   return (
     <div className="w-full">
       <div className="w-full px-10 py-3 flex items-center justify-center bg-secondary">
         <div className="w-full flex items-center justify-between">
           <h3 className="font-bold text-lg text-zinc-700">Stocks</h3>
+          <StockSearchBar stock={stock} />
           <AddStock>
             <Button>Add Stock</Button>
           </AddStock>
