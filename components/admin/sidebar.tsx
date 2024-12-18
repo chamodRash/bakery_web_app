3f4e0b2133790247fbc2f3e60fcc22d8ef44a2b5
Chamod Rashmika <cnilwakka@gmail.com>
2024-12-18 11:45:24 +0530

    reuploading sidebar.tsx

---
+++ components/admin/sidebar.tsx
@@ -0,0 +1,128 @@
+"use client";
+
+import React, { useState } from "react";
+// import { Sidebar, SidebarBody, SidebarLink } from "@/components/ui/sidebar";
+import {
+  IconArrowLeft,
+  IconBrandTabler,
+  IconSettings,
+  IconUserBolt,
+} from "@tabler/icons-react";
+import Link from "next/link";
+import {
+  HandCoins,
+  Layers3,
+  PackageSearch,
+  TrendingUp,
+  User,
+  UsersRound,
+} from "lucide-react";
+import Image from "next/image";
+import { cn } from "@/lib/utils";
+
+export default function AdminSidebar({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const links = [
+    {
+      label: "Dashboard",
+      href: "/admin",
+      icon: (
+        <IconBrandTabler className="text-neutral-700 dark:text-neutral-200 h-5 w-5 flex-shrink-0" />
+      ),
+    },
+    {
+      label: "POS",
+      href: "/admin/pos",
+      icon: (
+        <HandCoins className="text-neutral-700 dark:text-neutral-200 h-5 w-5 flex-shrink-0" />
+      ),
+    },
+    {
+      label: "Products",
+      href: "/admin/products",
+      icon: (
+        <PackageSearch className="text-neutral-700 dark:text-neutral-200 h-5 w-5 flex-shrink-0" />
+      ),
+    },
+    {
+      label: "Stock",
+      href: "/admin/stock",
+      icon: (
+        <Layers3 className="text-neutral-700 dark:text-neutral-200 h-5 w-5 flex-shrink-0" />
+      ),
+    },
+    {
+      label: "Employees",
+      href: "/admin/employees",
+      icon: (
+        <UsersRound className="text-neutral-700 dark:text-neutral-200 h-5 w-5 flex-shrink-0" />
+      ),
+    },
+    {
+      label: "Charts",
+      href: "/admin/charts",
+      icon: (
+        <TrendingUp className="text-neutral-700 dark:text-neutral-200 h-5 w-5 flex-shrink-0" />
+      ),
+    },
+    {
+      label: "Settings",
+      href: "/admin/settings",
+      icon: (
+        <IconSettings className="text-neutral-700 dark:text-neutral-200 h-5 w-5 flex-shrink-0" />
+      ),
+    },
+    {
+      label: "Logout",
+      href: "/logout",
+      icon: (
+        <IconArrowLeft className="text-neutral-700 dark:text-neutral-200 h-5 w-5 flex-shrink-0" />
+      ),
+    },
+  ];
+  const [open, setOpen] = useState(false);
+
+  return (
+    <div
+      className={cn(
+        "rounded-md flex flex-col md:flex-row bg-gray-100 dark:bg-neutral-800 w-full flex-1 border border-neutral-200 dark:border-neutral-700 overflow-hidden",
+        "h-screen"
+      )}>
+      {/*<Sidebar open={open} setOpen={setOpen}>
+        <SidebarBody className="justify-between gap-10">
+          <div className="flex flex-col flex-1 overflow-y-auto overflow-x-hidden">
+            <Link
+              href="/admin"
+              className="font-normal flex space-x-2 items-center text-sm text-black py-1 relative z-20">
+              <Image src={"/logo.jpg"} width={100} height={100} alt="Logo" />
+            </Link>
+            <div className="mt-8 flex flex-col gap-2">
+              {links.map((link, idx) => (
+                <SidebarLink key={idx} link={link} />
+              ))}
+            </div>
+          </div>
+          <div>
+            <SidebarLink
+              link={{
+                label: "Manu Arora",
+                href: "/admin/profile",
+                icon: (
+                  <User
+                    size={10}
+                    className="rounded-full bg-zinc-500 text-white w-6 h-6 p-1"
+                  />
+                ),
+              }}
+            />
+          </div>
+        </SidebarBody>}
+      </Sidebar>
+      */}
+      {children}
+    </div>
+  );
+}
